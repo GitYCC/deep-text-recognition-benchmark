e513a40ae8f41d27568d61c37d46253cbf192a95
Yi-Chang Chen <ycc.tw.email@gmail.com>
2021-05-19 18:16:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 source chars_var.sh
 
-CUDA_VISIBLE_DEVICES=0 python3.7 train.py \
+CUDA_VISIBLE_DEVICES=0 python train.py \
 --train_data lmdb_data/training \
 --valid_data lmdb_data/real_world_train \
 --select_data synth_batch \
 --batch_ratio 1.0 \
---num_iter 300000000 \
 --valInterval 2000 \
 --character "$CHARS" \
 --batch_max_length 32 \
